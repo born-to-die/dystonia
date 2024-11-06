d0b3af0b7a382355e065b0c2635649614ad6af01
intbad <int4bad@gmail.com>
2024-11-06 13:28:35 +0500

    REF Extract callback functs copies

--- src/game/event/spawn_event.lua
+++ src/game/event/spawn_event.lua
@@ -4,14 +4,29 @@ function SpawnEvent:create()
 
     local obj = {}
     obj.timer = nil
-    obj.repeatTime = 0.1
+    obj.repeatTime = 10
     obj.tileChecker = TileChecker:create()
 
     -- @param Wall[] walls
     -- @param Player player
     function obj.init(walls, player)
 
-        obj.callbackFunction = function()
+        obj.randomAction = function()
+            local r = math.random(3)
+
+            if r == 1 then 
+                obj.deleteWall()
+            else
+                obj.createWall()
+            end
+        end
+
+        obj.deleteWall = function()
+            local selectedWall = math.random(#walls)
+            table.remove(walls, selectedWall)
+        end
+
+        obj.createWall = function()
             for i = 1, 100, 1 do
                 local wx = math.random(10) - 1
                 local wy = math.random(10) - 1
@@ -26,7 +41,7 @@ function SpawnEvent:create()
             end
         end
 
-        obj.timer = TIMER(obj.repeatTime, obj.callbackFunction)
+        obj.timer = TIMER(obj.repeatTime, obj.randomAction)
     end
 
   
@@ -35,7 +50,7 @@ function SpawnEvent:create()
         if not obj.timer.isExpired() then
             obj.timer.update(GameScene.DT)
         else
-            obj.timer = TIMER(obj.repeatTime, obj.callbackFunction)
+            obj.timer = TIMER(obj.repeatTime, obj.randomAction)
         end
     end
 
